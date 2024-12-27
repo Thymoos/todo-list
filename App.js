8e8a70ac6db50937f34b4373de3ab447d3f3cd7e
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-12-27 12:15:31 +0100

    Archive build of the first version

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 export default function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
